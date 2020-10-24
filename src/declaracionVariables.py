eac1fa5e5c9815045f4a888ad85c9f0a124a5fca
Juan Carlos Serrano Perez <juan.carlos.serrano.perez@everis.com>
2020-10-24 13:28:04 +0200

    Create user

---
+++ src/declaracionVariables.py
@@ -0,0 +1,31 @@
+import os
+
+import sqlite3 as sql
+
+from flask import Flask, redirect
+from flask import render_template
+from flask import jsonify
+from flask import request
+from flask import abort
+from flask import Response
+
+# Flask initialisation
+app = Flask(__name__)
+DEBUG = True
+
+#BBDD
+conn = sql.connect('database.db')
+print("Opened database successfully")
+# drop tables
+conn.execute('DROP TABLE IF EXISTS user')
+# create tables
+conn.execute('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT, rol INTEGER)')
+print("Table created successfully")
+conn.close()
+
+# insert test data
+with sql.connect("database.db") as con:
+    cur = con.cursor()
+    cur.execute("INSERT INTO user (email, password, rol) VALUES (?,?,?)",('a@a.com','1234','0') )
+    con.commit()
+con.close()
---
+++ src/server.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request, Response
+from declaracionVariables import *
+
+import io, time, os, json, re
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+    
+@app.route('/usuarios')
+def users():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from user")
+    rows = cur.fetchall()
+    print(rows)
+    return render_template("registro_usuario.html",rows = rows)
+
+@app.route('/rest/usuario', methods=['POST'])
+def POST_user():
+    #Insert user
+    email = request.form["email"]
+    password = request.form["password"]
+    rol = request.form["rol"]
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO user (email, password, rol) VALUES (?,?,?)",(email,password,rol) )
+        con.commit()
+    con.close()
+    return redirect("/usuarios", code=302)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 80))
+    app.run(host='0.0.0.0', port=port,debug=DEBUG)
