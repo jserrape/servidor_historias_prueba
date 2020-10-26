a34e648a036bcf1733e32642cbe182245519dd99
Juan Carlos Serrano Perez <juan.carlos.serrano.perez@everis.com>
2020-10-26 13:10:32 +0100

    Elimino la funcionalidad

--- src/server/app.py
+++ src/server/app.py
@@ -22,36 +22,12 @@ def users():
     print(rows)
     return render_template("registro_usuario.html",rows = rows)
 
-@app.route('/rest/usuario', methods=['PUT'])
+@app.route('/rest/usuario', methods=['POST'])
 def POST_user():
     print('Peticion a /rest/usuario')
     print(request.headers)
     print('------------------------')
-    #Insert user
-    email = request.form["email"]
-    password = request.form["password"]
-    rol = request.form["rol"]
-
-    #print(request.headers)
-    #print(request.__dict__)
-
-    print('-----')
-    print(email)
-    print(password)
-    print(rol)
-    print('------')
-
-    with sql.connect("database.db") as con:
-        try:
-            cur = con.cursor()
-            cur.execute("INSERT INTO user (email, password, rol) VALUES (?,?,?)",(email,password,rol) )
-            con.commit()
-        except:
-            print("Ya existe un usuario con ese email")
-            con.close()
-            return redirect("/usuarios", code=400)
-    con.close()
-    return redirect("/usuarios", code=201)
+    return 'ok'
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 80))
