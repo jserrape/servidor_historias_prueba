62d9a96a904a55a7b2a2234ce8af1a79f272247a
Juan Carlos Serrano Perez <juan.carlos.serrano.perez@everis.com>
2020-10-27 21:15:55 +0100

    Añado código de la peticion

--- src/server/app.py
+++ src/server/app.py
@@ -126,8 +126,10 @@ def login_user():
     cur.execute("SELECT COUNT(*) FROM user WHERE email = ? AND password = ?", (email,password))
     result=cur.fetchone()
     number_of_rows=result[0]
-    print(number_of_rows)
-
+    if number_of_rows == 1:
+        respons.status_code = 201
+    else:
+        respons.status_code = 400
     return respons
 
 if __name__ == '__main__':
